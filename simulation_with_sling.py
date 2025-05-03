a7470102ab7f12892ebb2208587211f2c38576e5
Liam Greenlee <liam.a.greenlee@nasa.gov>
2025-05-03 11:09:13 -0400

    Add sling sim

--- simulation_with_sling.py
+++ simulation_with_sling.py
@@ -7,67 +7,68 @@ import matplotlib.animation as animation
 # --- Symbolic Setup ---
 t = sp.symbols('t')
 # Generalized coordinates
-theta = sp.Function('theta')(t)
-dtheta = theta.diff(t)
+theta1 = sp.Function('theta1')(t)
+theta2 = sp.Function('theta2')(t)
+dtheta1 = theta1.diff(t)
+dtheta2 = theta2.diff(t)
 # Physical parameters
-m_cw, m_p, m_arm, L1, L2, g, c1, c2, c3 = sp.symbols('m_cw m_p m_arm L1 L2 g c1 c2 c3')
+m_cw, m_p, m_arm, L1, L2, L3, g, c1, c2, c3 = sp.symbols('m_cw m_p m_arm L1 L2 L3 g c1 c2 c3')
 
 # Arm length
 L = L1 + L2
 
 # Counterweight and projectile positions
-x_cw = sp.Matrix([[0],[L1*sp.sin(theta)]])
-x_p = sp.Matrix([[-L*sp.cos(theta)], [-L2*sp.sin(theta)]])
+x_cw = sp.Matrix([[0],[L1*sp.sin(theta1)]])
+x_end = sp.Matrix([[-L*sp.cos(theta1)], [-L2*sp.sin(theta1)]])
+x_pivot = sp.Matrix([[-L1*sp.cos(theta1)],[0]])
+x_p = x_end + sp.Matrix([[L3*sp.sin(theta2)],[-L3*sp.cos(theta2)]])
+
+Rz = sp.Matrix([[sp.cos(theta1), -sp.sin(theta1)],[sp.sin(theta1), sp.cos(theta1)]])
+
+lcom = -(L2-L1)/2
+x_arm = Rz*sp.Matrix([[lcom],[0]])
+
+dx_cw = x_cw.diff(t)
 dx_p = x_p.diff(t)
-
-
-Rz = sp.Matrix([[sp.cos(theta), -sp.sin(theta)],[sp.sin(theta), sp.cos(theta)]])
-
-M = m_p + m_arm + m_cw
-# Center of mass distance from pivot
-acom = (0*m_cw - L*m_p - (L/2)*m_arm)/M+L1
-# COM position wrt pivot
-arm_com = Rz * sp.Matrix([[acom],[0]]) 
-
+dx_arm = x_arm.diff(t)
+dx_pivot = x_pivot.diff(t)
 
 # Moment of inertia about pivot using parallel axis theorem
 # I_arm = (1/12) * m_arm * L**2 + m_arm * acom**2
 I_arm = (1/12) * m_arm * L**2
-I_acom = I_arm + m_arm**acom**2
-# I_acom = m_cw*L1**2 + m_p*L2**2 + I_arm
-I_pivot =  m_cw*L1**2 + m_p*L2**2 + I_arm + I_acom
+I_pivot = I_arm + m_arm**lcom**2
 
 # --- Kinetic Energy ---
 # arm translation velocity + arm angular velocity (where arm includes the projectile and counterweight masses)
-T = (1/2)*M*sp.diff(arm_com[0],t)**2 + (1/2)*M*sp.diff(arm_com[1],t)**2 + (1/2)*(I_pivot)*dtheta**2
+# T = (1/2)*M*sp.diff(arm_com[0],t)**2 + (1/2)*M*sp.diff(arm_com[1],t)**2 + (1/2)*(I_pivot)*dtheta**2
+T = ((1/2)*m_p*dx_p.T*dx_p + (1/2)*m_cw*dx_cw.T*dx_cw + (1/2)*m_arm*dx_arm.T*dx_arm)[0] + (1/2)*I_pivot*dtheta1**2
 
 # --- Potential Energy ---
 # Gravity affects both the counterweight and projectile
-V_cw = m_cw * g *(L1 * sp.sin(theta))  # Counterweight potential energy
-V_proj = m_p * g *(-L2 * sp.sin(theta))  # Projectile potential energy
-V_arm = m_arm * g * arm_com[1] # Arm center of mass potential energy
-
-V = V_cw + V_proj + V_arm
+V_cw = m_cw*g*x_cw[1]
+V_p = m_p*g*x_p[1]
+V_arm = m_p*g*x_arm[1]
+V = V_cw + V_p + V_arm
 
 # --- Lagrangian ---
 Lagr = T - V
 
-# Pivot position and velocity used for frictional damping
-x_pivot = -L1*sp.cos(theta)
-dx_pivot = x_pivot.diff(t)
 
-eq = sp.Eq(sp.diff(sp.diff(Lagr,dtheta),t) - sp.diff(Lagr,theta) - c1*dx_pivot + c2*dtheta - c3*sp.diff(x_cw[1], t), 0)
-ddot_theta = sp.solve(eq, sp.diff(theta,t,t))[0]
+eq1 = sp.Eq(sp.diff(sp.diff(Lagr,dtheta1),t) - sp.diff(Lagr,theta1) - c1*dx_pivot[0] + c2*dtheta1 - c3*sp.diff(x_cw[1], t), 0)
+eq2 = sp.Eq(sp.diff(sp.diff(Lagr,dtheta2),t) - sp.diff(Lagr,theta2) - c2*dtheta2, 0)
+ddot_theta1 = sp.solve(eq1, sp.diff(theta1,t,t))[0]
+ddot_theta2 = sp.solve(eq2, sp.diff(theta2,t,t))[0]
 
 # --- Substitute parameter values ---
 params = {
     m_cw: 150,
-    m_p: 0.4,
+    m_p: 0.5,
     m_arm: 0,
-    L1: 1,
+    L1: 0.5,
     L2: 3.0,
+    L3: 2.5,
     g: 9.81,
-    c1: 0.5,  # Friction coefficient (change as needed)
+    c1: 0.3,  # Friction coefficient (change as needed)
     c2: 0,  # Friction coefficient (change as needed)
     c3: 0
 }
@@ -75,55 +76,53 @@ params = {
 # assume 2in by 2in (5cm by 5cm) wood arm
 params[m_arm] = (0.03)**2*(L.subs(params))*500
 # The damping factor due to normal force of friction is mu*m*g. Here c1 is mu
+M = m_cw + m_p + m_arm
 params[c1] = M.subs(params)*params[g]*params[c1]
 
-def simulate(params, release=True):
-    eom = ddot_theta.subs(params)
-    # M_func = sp.lambdify((theta), M.subs(params), 'numpy')
-    # Q_func = sp.lambdify((theta, dtheta), Q.subs(params), 'numpy')
+def simulate(params, t_release = None):
+    eom1 = ddot_theta1.subs(params)
+    eom2 = ddot_theta2.subs(params)
     # --- ODE System ---
-    def trebuchet_ode(t, y, eom):
-        th, dth = y
-        ddth = eom.subs({theta: th, dtheta: dth})
-        return [dth, ddth]
+    def trebuchet_ode(t, y, eom1, eom2):
+        th1, dth1, th2, dth2 = y
+        ddth1 = eom1.subs({theta1: th1, dtheta1: dth1, theta2: th2, dtheta2: dth2}).doit()
+        ddth2 = eom2.subs({theta1: th1, dtheta1: dth1, theta2: th2, dtheta2: dth2}).doit()
+        return [dth1, ddth1, dth2, ddth2]
 
     # --- Simulate ---
     # y0 = [np.pi/4, 0]  # initial theta, dtheta, x, dx
-    d_theta = sp.solve(sp.Eq(dx_pivot, -np.sqrt(2*(9.81)*0)), dtheta)[0].subs(params)
-    y0 = [np.pi/4, d_theta.subs({theta: np.pi/4})]
+    d_theta = sp.solve(sp.Eq(dx_cw[1], -np.sqrt(2*(9.81)*0.1)), dtheta1)[0].subs(params)
+    y0 = [np.pi/4, d_theta.subs({theta1: np.pi/4}), 0, 0]
     t0 = 0
     t_end = 4
 
-    t_release = t_end
-    if release:
-        t_release = 0.25
-
-    sol1 = solve_ivp(lambda t, y: trebuchet_ode(t, y, eom), [t0, t_release], y0, t_eval=np.linspace(t0, t_release, int(1000*(t_release/t_end))))
-
-    if release:
+    if t_release:
+        sol1 = solve_ivp(lambda t, y: trebuchet_ode(t, y, eom1, eom2), [t0, t_release], y0, t_eval=np.linspace(t0, t_release, int(1000*(t_release/t_end))))
         y_release = sol1.y[:,-1]
         release_ind = len(sol1.t)
         params[m_p] = 0
-        eom = ddot_theta.subs(params)
-        sol2 = solve_ivp(lambda t,y: trebuchet_ode(t, y, eom), [t_release, t_end], y_release, t_eval=np.linspace(t_release, t_end,int(1000*(1-t_release/t_end))))
+        eom1 = ddot_theta1.subs(params)
+        eom2 = ddot_theta2.subs(params)
+        sol2 = solve_ivp(lambda t,y: trebuchet_ode(t, y, eom1, eom2), [t_release, t_end], y_release, t_eval=np.linspace(t_release, t_end,int(1000*(1-t_release/t_end))))
 
         t_full = np.hstack((sol1.t, sol2.t[1:]))
         y_full = np.hstack((sol1.y, sol2.y[:, 1:]))
     else:
+        sol1 = solve_ivp(lambda t, y: trebuchet_ode(t, y, eom1, eom2), [t0, t_end], y0, t_eval=np.linspace(t0, t_end, 1000))
         t_full = sol1.t
         y_full = sol1.y
         max_vel_ind = np.argmax(np.abs(y_full[1,:]))
         y_release = y_full[:,max_vel_ind]
     return t_full, y_full, y_release
 
-def state_plot(t,y, params, plot = True):
+def state_plot(t, y, params, plot = True):
 
     x_p_val = np.array([
-        np.array(x_p.subs(params).subs({theta: y[0, i], dtheta: y[1, i]})).astype(float)
+        np.array(x_p.subs(params).subs({theta1: y[0, i], dtheta1: y[1, i], theta2: y[2,i], dtheta2: y[3,i]})).astype(float)
         for i in range(y.shape[1])
     ]).reshape(y.shape[1],2)
     dx_p_val = np.array([
-        np.array(dx_p.subs(params).subs({theta: y[0, i], dtheta: y[1, i]})).astype(float)
+        np.array(dx_p.subs(params).subs({theta1: y[0, i], dtheta1: y[1, i], theta2: y[2,i], dtheta2: y[3,i]})).astype(float)
         for i in range(y.shape[1])
     ]).reshape(y.shape[1], 2)
 
@@ -203,50 +202,59 @@ def state_plot(t,y, params, plot = True):
 
         ax_left.set_xlabel('Time (s)')
         plt.show()
-    return max_range_ind, optimal_release_state, x_p_val[max_range_ind], dx_p_val[max_range_ind], range_vals[max_range_ind]
+    return max_range_ind, optimal_release_state, x_p_val[max_range_ind], dx_p_val[max_range_ind], range_vals[max_range_ind], t[max_range_ind]
 
 def plot_animation(t, y, y_release, params):
     # --- Real-time Animation ---
     # Extract data
     theta_vals = y[0]
 
-    # Geometry
-    L1_val = params[sp.Symbol('L1')]
-    L2_val = params[sp.Symbol('L2')]
 
     # Compute point positions
     def get_arm_coords(i):
-        cw = np.array([0, L1_val * np.sin(theta_vals[i])])
-        pivot = np.array([-L1_val*np.cos(theta_vals[i]), 0])
-        prj = np.array([-(L1_val + L2_val) * np.cos(theta_vals[i]), -(L2_val) * np.sin(theta_vals[i])])
-        return cw, pivot, prj
+        state = {theta1: y[0,i], dtheta1: y[1,i], theta2: y[2,i], dtheta2: y[3,i]}
+        cw = np.array(x_cw.subs(params).subs(state))
+        pivot = np.array(x_pivot.subs(params).subs(state))
+        prj = np.array(x_p.subs(params).subs(state))
+        end = np.array(x_end.subs(params).subs(state))
+        return cw, pivot, end, prj
 
     # --- Compute Projectile Velocity ---
     theta_vals = y[0]
     dtheta_vals = y[1]
 
-    L1_val = params[L1]
-    L2_val = params[L2]
-
     # --- Setup Animation + Velocity Plot ---
     fig, ax_anim  = plt.subplots(1, 1, figsize=(10, 10))
 
     # Animation plot
-    ax_anim.set_xlim(-6, 40)
-    ax_anim.set_ylim(-6, 20)
+    ax_anim.set_xlim(-6, 70)
+    ax_anim.set_ylim(-6, 50)
     ax_anim.grid()
     ax_anim.set_aspect('equal')
     ax_anim.set_title("Floating Arm Trebuchet Animation with Pivot Friction")
     ax_anim.set_xlabel("x (m)")
     ax_anim.set_ylabel("y (m)")
-    x_p_val = np.array(x_p.subs(params).subs({theta: y_release[0], dtheta: y_release[1]})).astype(float)
-    dx_p_val = np.array(dx_p.subs(params).subs({theta: y_release[0], dtheta: y_release[1]})).astype(float)
 
+    x_p_val = y_release[0]
+    dx_p_val= y_release[1]
+    t_traj = np.linspace(0, 20, 1000).reshape(1, 1000)
 
-    # Compute full trajectory
-    t_traj = np.linspace(0,t[-1], 1000)
-    x_p_traj = x_p_val + dx_p_val*t_traj + np.array([[0],[0.5*-9.81]])*t_traj**2
-    ax_anim.plot(x_p_traj[0,:], x_p_traj[1,:])
+    # Assuming the acceleration is gravity acting in the second dimension
+    acceleration = np.array([0, -9.81])  # gravity acceleration in the second dimension
+
+    # Reshape t_traj to be (1000, 1) to allow broadcasting with acceleration
+    t_traj_reshaped = t_traj.T  # Now t_traj_reshaped is shape (1000, 1)
+
+    # Calculate the trajectory
+    x_p_traj = x_p_val + dx_p_val * t_traj_reshaped + 0.5 * acceleration * t_traj_reshaped**2
+        
+    # Find index where y-position becomes non-positive
+    y_vals = x_p_traj[:,1]
+    valid_idx = np.where(y_vals >= 0)[0]
+    if valid_idx.size > 0:
+        last_valid = valid_idx[-1]
+        ax_anim.plot(x_p_traj[:last_valid+1,0], y_vals[:last_valid+1])
+
     arm_line, = ax_anim.plot([], [], 'o-', lw=3)
 
     # Init function
@@ -256,9 +264,9 @@ def plot_animation(t, y, y_release, params):
 
     # Animation update
     def update(frame):
-        cw, pivot, prj = get_arm_coords(frame)
-        xs = [cw[0], pivot[0], prj[0]]
-        ys = [cw[1], pivot[1], prj[1]]
+        cw, pivot, end, prj = get_arm_coords(frame)
+        xs = [cw[0], pivot[0], end[0], prj[0]]
+        ys = [cw[1], pivot[1], end[1], prj[1]]
 
         arm_line.set_data(xs, ys)
         return (arm_line,)
@@ -299,28 +307,7 @@ def plot_trajectories(p_pos, p_vel):
     # ax.set_aspect('equal')
     plt.show()
 
-
-release_states = []
-release_velocites = []
-lengths = np.linspace(1,5,10)
-x_p_pos = []
-x_p_vel = []
-max_ranges = []
-for length in lengths:
-    params[L1] = length
-    params[L2] = 4-length
-    t_full, y_full, y_release = simulate(params, False)
-
-    max_ind, opt_state, x_p_rls, dx_p_rls, max_range = state_plot(t_full, y_full, params, False)
-    max_ranges.append(max_range)
-    # plot_animation(t_full, y_full, opt_state, params)
-    release_states.append(opt_state)
-    x_p_pos.append(x_p_rls)
-    x_p_vel.append(dx_p_rls)
-
-# state_plot()
-# plot_animation(t_full, y_full, y_release)
-plot_trajectories(x_p_pos,x_p_vel)
-plt.figure()
-plt.plot(lengths, max_ranges)
-plt.show()
+t_full, y_full, _ = simulate(params)
+max_range_ind, optimal_release_state, x_p_val, dx_p_val, range, release_time = state_plot(t_full, y_full, params)
+# t_full, y_full, _ = simulate(params, release_time)
+plot_animation(t_full, y_full, [x_p_val, dx_p_val], params)
